2014abb458a4f6643d2bb4e5956cea234260738a
Ajay2004123 <patelasp16@gmail.com>
2026-02-22 09:46:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+const PORT = process.env.PORT || 5000;
 
 // Ensure uploads dir exists
 const uploadsDir = path.join(__dirname, 'public', 'uploads');
